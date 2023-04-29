706b510922bab7b702c3557fcc448174491dc688
Guruprasad6360 <16bhagwatguruprasad@gmail.com>
2023-04-29 23:42:05 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"welcome",component:WelcomeComponent},
+  {path:'', redirectTo:'welcome', pathMatch:"full"},
+  {path:"question",component:QuestionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
